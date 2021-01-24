2bc06e6f235c850db2c7f898c9dafa76f39cb712
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-01-24 17:56:34 +0900

    feat: Provide static file

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,8 @@
 // modules
 const Koa = require('koa');
 const Router = require('koa-router');
+const serve = require('koa-static');
+const path = require('path');
 
 const app = new Koa();
 const router = new Router();
@@ -12,6 +14,9 @@ router.get('/payment', (ctx, next) => {
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
+
+// 정적 파일 제공
+app.use(serve(path.join(__dirname, '../../client')));
 
 app.listen(4000, () => {
     console.log('Listening to port 4000');
