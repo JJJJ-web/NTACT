dafbd0e5acaa8cb56c219f350f4510ec3546cac0
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-01-26 22:49:40 +0900

    build: Update to solve linebreak issue

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
         'no-multiple-empty-lines': 0,
         'space-before-function-paren': 0,
         'eol-last': 0,
+        'linebreak-style': 0,
     },
     'env': {
         'es6': true,
