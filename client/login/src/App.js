38bfcd8ffb0ba92c983de9bc7f8eb1cb11f1547b
9SuJin8 <sstt9884@gmail.com>
2021-01-28 00:24:18 +0900

    feat: Rendering start page

---
+++ client/login/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Login from './component/Login';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Main Home Page</h3>
+                <Login />
+            </div>
+        );
+    }
+}
+
+export default App;
