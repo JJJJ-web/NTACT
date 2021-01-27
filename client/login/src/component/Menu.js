ebcff41ec091e48da9b25b2d5c1a8e876aa1cfaa
9SuJin8 <sstt9884@gmail.com>
2021-01-28 00:23:00 +0900

    feat: Menu page

---
+++ client/login/src/component/Menu.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Menu extends Component {
+    render() {
+        return(
+            <div>
+                <h3>메뉴 화면 입니다.</h3>
+            </div>
+        );
+    }
+}
+
+export default Menu;
