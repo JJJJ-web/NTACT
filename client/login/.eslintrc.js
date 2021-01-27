0498148223a3eeab5074380a58d0ec0050a50178
9SuJin8 <sstt9884@gmail.com>
2021-01-28 00:40:20 +0900

    build: Update to solve linebreak issue

---
+++ client/login/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  'extends': 'google',
+  'rules': {
+    'indent': [
+      'error',
+      4,
+    ],
+    'semi': [
+      'error',
+      'always',
+    ],
+    'no-trailing-spaces': 0,
+    'keyword-spacing': 0,
+    'no-unused-vars': 1,
+    'no-multiple-empty-lines': 0,
+    'space-before-function-paren': 0,
+    'eol-last': 0,
+    'linebreak-style': 0,
+    'require-jsdoc': 0,
+  },
+  'env': {
+    'es6': true,
+  },
+};
