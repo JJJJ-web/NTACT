c44e6e6689b52c8904e90486cec663a0ec8c4598
jokbalkiller <55704603+jokbalkiller@users.noreply.github.com>
2021-01-28 23:22:49 +0900

    docs: models/index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,19 +1,33 @@
 'use strict';
 
-//const fs = require('fs');
+var fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 
-//const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
+fs
+  .readdirSync(__dirname)
+  .filter(function(file) {
+    return (file.indexOf(".") !== 0) && (file !== "index.js");
+  })
+  .forEach(function(file) {
+    var model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+    console.log('model.name:' + model.name);  // 테스트로그 model명..
+  });
+
+Object.keys(db).forEach(function(modelName) {
+  if ("associate" in db[modelName]) {
+    db[modelName].associate(db);
+  }
+});
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
-db.User = require('./user')(sequelize, Sequelize);
 
 module.exports = db;
