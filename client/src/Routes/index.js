9b0cca118d098e067b7441f4de9d21c53a01f867
9SuJin8 <sstt9884@gmail.com>
2021-02-01 00:31:38 +0900

    feat: Add pages that can be moved

--- client/src/Routes/index.js
+++ client/src/Routes/index.js
@@ -1,5 +1,8 @@
 export {default as Home} from '../component/Home';
 export {default as Login} from '../component/Login';
-export {default as Menu} from '../component/Menu';
 export {default as Cart} from '../component/Cart';
 export {default as Payment} from '../component/Payment';
+export {default as Coffee} from '../pages/Coffee';
+export {default as Ade} from '../pages/Ade';
+export {default as Shake} from '../pages/Shake';
+export {default as MilkBeverage} from '../pages/MilkBeverage';
