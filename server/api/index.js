49458afa2743d65fe907e8027d77a176bb046675
jokbalkiller <55704603+jokbalkiller@users.noreply.github.com>
2021-02-01 17:12:30 +0900

    feat: create api/index.js

---
+++ server/api/index.js
@@ -0,0 +1,9 @@
+const Router = require('koa-router');
+
+const api = new Router();
+const users = require('./user')
+
+// api/users/~ 경로들은 users 모듈에서 사용
+api.use('/users', users.routes());
+
+module.exports = api;
