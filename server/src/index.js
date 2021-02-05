4172ebe6fac2955825c49e667fe982265be8feeb
jokbalkiller <55704603+jokbalkiller@users.noreply.github.com>
2021-02-05 17:23:28 +0900

    feat: axios 모듈 설치 & 카카오서버에 GET 요청보내 유저 DB 받아오기

--- server/src/index.js
+++ server/src/index.js
@@ -12,8 +12,8 @@ const app = new Koa();
 const router = new Router();
 
 // HTTP header 부분을 text로 변환해줍니다. ctx.body로 접근가능합니다.
-// json, text 형태로 반환해줍니다.
-app.use(bodyParser({enableTypes: ['json', 'text']}));
+// json, text, form 형태로 반환해줍니다.
+app.use(bodyParser({enableTypes: ['json', 'text', 'form']}));
 
 // /api/~ 경로들은 api 모듈에서 사용
 router.use('/api', api.routes());
