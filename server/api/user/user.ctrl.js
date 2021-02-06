d068f1a418e2c5a8e658b122470b0eb7264280a4
jokbalkiller <55704603+jokbalkiller@users.noreply.github.com>
2021-02-06 23:59:34 +0900

    feat: 프론트에 200 response & 토큰 전달

--- server/api/user/user.ctrl.js
+++ server/api/user/user.ctrl.js
@@ -7,46 +7,45 @@ exports.index = (ctx) => {
 };
 
 exports.loginKakao = async (ctx) => {
+    let jwtToken;
     // header로 보낸 request payload를 접근하려면 body로 접근
     const kakaoToken = ctx.request.body.headers.Authorization;
-    //console.log(`access token : ${kakaoToken}`);
+    // console.log(`access token : ${kakaoToken}`);
     axios.get('https://kapi.kakao.com/v2/user/me', {
         headers: {
             'Authorization': `Bearer ${kakaoToken}`,
         },
     })
-        .then((response) => {
-            const kakaoUserDB = response.data;
-
+        .then((res) => {
+            const kakaoUserDB = res.data;
             /*
             DB에 해당 id를 가지고 있는것을 찾아본다.
             findOrCreate 메서드는 검색되었거나 또는 생성된 객체를 포함한 배열, 그리고 boolean값을 반환합니다. 여기서 boolean값은, 새 객체가 생성되었을 경우 true, 그렇지 않을 경우 false입니다.
             */
             kakaoUserModel.findOrCreate({
-                where: { userid: `${kakaoUserDB.id}` },
-                //없을경우 defualts에 있는 정보로 user가 생성된다.
+                where: {userid: `${kakaoUserDB.id}`},
+                // 없을경우 defualts에 있는 정보로 user가 생성된다.
                 defaults: {
                     username: `${kakaoUserDB.properties.nickname}`,
                     email: `${kakaoUserDB.kakao_account.email}`,
                     gender: `${kakaoUserDB.kakao_account.gender}`,
                     profile_image: `${kakaoUserDB.properties.profile_image}`,
                     age: `${kakaoUserDB.kakao_account.age_range}`,
-                }
-            })
+                },
+            });
+
             console.log(`유저DB 저장 완료`);
 
-            //jwt 토큰 생성
-            const jwtToken = jwt.sign({
-                name: `${kakaoUserDB.properties.nickname}`
+            // jwt 토큰 생성
+            jwtToken = jwt.sign({
+                name: `${kakaoUserDB.properties.nickname}`,
             }, 'qlalfqjsgh');
-            // 프론트로 jwt토큰 보내기
-            ctx.status = 200;
-            ctx.body = jwtToken;
         })
         .catch(function (error) {
             console.log(error);
-        })
-
-
+        });
+   
+    ctx.response.body = jwtToken;
+    ctx.response.status = 200;
 };
 
