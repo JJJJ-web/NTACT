a82152c7f6426aa82d792e1cb37fe30e655ec9e7
jokbalkiller <55704603+jokbalkiller@users.noreply.github.com>
2021-02-08 18:06:21 +0900

    feat: res에 유저 정보 jwtToken 으로 보내기

--- server/api/user/user.ctrl.js
+++ server/api/user/user.ctrl.js
@@ -7,19 +7,28 @@ exports.index = (ctx) => {
 };
 
 exports.loginKakao = async (ctx) => {
-    let jwtToken;
-    let username;
-
     // header로 보낸 request payload를 접근하려면 body로 접근
     const kakaoToken = ctx.request.body.headers.Authorization;
-    // console.log(`access token : ${kakaoToken}`);
-    axios.get('https://kapi.kakao.com/v2/user/me', {
+    let jwtToken;
+
+    // kakao 서버에 요청하여 유저정보 가져온후 DB에 저장
+    await axios.get('https://kapi.kakao.com/v2/user/me', {
         headers: {
             'Authorization': `Bearer ${kakaoToken}`,
         },
     })
         .then((res) => {
             const kakaoUserDB = res.data;
+            
+            // 토큰 생성
+            jwtToken = jwt.sign(
+                {
+                    username: kakaoUserDB.properties.nickname,
+                    id: kakaoUserDB.id, // 유저 정보
+                },
+                'qlalfqjsgh', // secrec Key
+            );    
+            
             /*
             DB에 해당 id를 가지고 있는것을 찾아본다.
             findOrCreate 메서드는 검색되었거나 또는 생성된 객체를 포함한 배열, 그리고 boolean값을 반환합니다. 여기서 boolean값은, 새 객체가 생성되었을 경우 true, 그렇지 않을 경우 false입니다.
@@ -35,23 +44,11 @@ exports.loginKakao = async (ctx) => {
                     age: `${kakaoUserDB.kakao_account.age_range}`,
                 },
             });
-
-            console.log(`유저DB 저장 완료`);
-
-            // jwt 토큰 생성
-            jwtToken = jwt.sign({
-                name: `${kakaoUserDB.properties.nickname}`,
-            }, 'qlalfqjsgh');
-            
-            // 토큰으로 보내기 성공시 삭제
-            username = `${kakaoUserDB.properties.nickname}`;
         })
         .catch(function (error) {
             console.log(error);
         });
-        
-    // 이름을 직접 보내는 방식
-    ctx.response.body = {name: `${username}`};
-    ctx.response.status = 200;
+    
+    ctx.body = {jwtToken: jwtToken};
+    ctx.status = 200;
 };
-
