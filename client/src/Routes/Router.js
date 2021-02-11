49335773877dd62092848c3eb837d17beb379a61
truebliss <s1871323@gmail.com>
2021-02-11 21:55:34 +0900

    feat: 결제 추가
    일단 백연동 테스트는 가능
    pg사 선택 등 antd 스타일적용 안돼서
    Payment/index.js 코드 수정 예정

--- client/src/Routes/Router.js
+++ client/src/Routes/Router.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
-import {Home, FinalCart, Payment, Coffee, Shake, Ade, MilkBeverage} from './index';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import {Home, FinalCart, Coffee, Shake, Ade, MilkBeverage, Payment, PaymentResult} from './index';
 
 const Router = () => (
     <Switch>
         <Route exact path="/" component={Home} />
         <Route path='/finalcart' component={FinalCart} />
-        <Route path='/payment' component={Payment} />
         <Route path='/coffee' component={Coffee} />
         <Route path='/shake' component={Shake} />
         <Route path='/ade' component={Ade} />
         <Route path='/milkbeverage' component={MilkBeverage} />
+        <Route exact path="/payment" component={Payment} />
+        <Route exact path="/payment/result" component={PaymentResult} />
     </Switch>
 );
 
--- client/src/Routes/index.js
+++ client/src/Routes/index.js
@@ -1,7 +1,9 @@
 export {default as Home} from '../component/Home';
 export {default as FinalCart} from '../component/FinalCart';
-export {default as Payment} from '../component/Payment';
+// export {default as Payment} from '../component/Payment';
 export {default as Coffee} from '../pages/Coffee';
 export {default as Ade} from '../pages/Ade';
 export {default as Shake} from '../pages/Shake';
 export {default as MilkBeverage} from '../pages/MilkBeverage';
+export {default as Payment} from '../Payment/index';
+export {default as PaymentResult} from '../PaymentResult/index';
