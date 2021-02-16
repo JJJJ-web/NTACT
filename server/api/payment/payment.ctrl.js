d62356d60d8bd2a87a1aef81da4e3407a0e2fe0e
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-02-16 15:39:51 +0900

    feat: Get payment data

--- server/api/payment/payment.ctrl.js
+++ server/api/payment/payment.ctrl.js
@@ -3,6 +3,8 @@ const paymentConfig = require('../../config/payment-config.json');
 
 exports.complete = async (ctx) => {
     try {
+        const {impUid} = ctx.request.body;
+
         const getToken = await axios({ // access token 발급
             url: 'https://api.iamport.kr/users/getToken',
             method: 'post',
@@ -12,6 +14,15 @@ exports.complete = async (ctx) => {
                 imp_secret: paymentConfig.imp_secret,
             },
         });
+        const {accessToken} = getToken.data.response; // 인증 토큰
+
+        const getPaymentData = await axios({ // 결제 정보 조회
+            url: `https://api.iamport.kr/payments/\${impUid}`,
+            method: 'get',
+            headers: {'Authorization': accessToken},
+        });
+        const paymentData = getPaymentData.data.resource;
+        console.log(paymentData);
     } catch (e) {
         ctx.status = 400;
         ctx.body = e;
