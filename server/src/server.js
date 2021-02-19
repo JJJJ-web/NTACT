fddf16fd309b78f4f90cbebf7c2321f888250599
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-02-19 10:54:04 +0900

    refactor: Modify port setting for future deploy

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,8 @@ const syncDb = require('./sync-db');
 // DB 연동 후 서버 구동
 syncDb().then((_)=> {
     console.log('Sync database!');
-    app.listen(4000, () => {
-        console.log(`Server is running on 4000 port`);
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on ${PORT} port`);
     });
 });
