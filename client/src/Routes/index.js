aa25a8b10be9e2dd9305e447ed72acd101085deb
9SuJin8 <sstt9884@gmail.com>
2021-02-26 22:09:05 +0900

    feat: 회원, 비회원 구분 로그인 페이지 추가

--- client/src/Routes/index.js
+++ client/src/Routes/index.js
@@ -1,6 +1,6 @@
 export {default as Home} from '../component/Home';
+export {default as Login} from '../component/Login';
 export {default as FinalCart} from '../component/FinalCart';
-// export {default as Payment} from '../component/Payment';
 export {default as Coffee} from '../pages/Coffee';
 export {default as Ade} from '../pages/Ade';
 export {default as Shake} from '../pages/Shake';
