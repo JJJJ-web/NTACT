39d691a9ff00ef1a027509d04227b88c9fce0a59
truebliss <s1871323@gmail.com>
2021-02-26 22:26:50 +0900

    chore: edit url

--- client/src/component/Home.js
+++ client/src/component/Home.js
@@ -5,7 +5,7 @@ import {Button} from 'antd';
 function Home() {
     return (
         <div>
-            <img src='/ntact512.png'/>
+            <img src='./ntact512.png'/>
             <br />
             <Link to='/login'>
                 <Button>회원</Button>
