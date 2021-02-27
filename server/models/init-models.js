91fc902570589fb6929e26a402533b423b3d9f60
jokbalkiller <55704603+jokbalkiller@users.noreply.github.com>
2021-02-28 01:53:01 +0900

    feat: GoogleLogin  & User DB 구조 수정

--- server/models/init-models.js
+++ server/models/init-models.js
@@ -1,7 +1,6 @@
 var DataTypes = require("sequelize").DataTypes;
 var _category = require("./category");
 var _dev_category = require("./dev_category");
-var _dev_kakao_user = require("./dev_kakao_user");
 var _dev_menu = require("./dev_menu");
 var _dev_merchant = require("./dev_merchant");
 var _dev_orders = require("./dev_orders");
@@ -12,7 +11,6 @@ var _user = require("./user");
 function initModels(sequelize) {
   var category = _category(sequelize, DataTypes);
   var dev_category = _dev_category(sequelize, DataTypes);
-  var dev_kakao_user = _dev_kakao_user(sequelize, DataTypes);
   var dev_menu = _dev_menu(sequelize, DataTypes);
   var dev_merchant = _dev_merchant(sequelize, DataTypes);
   var dev_orders = _dev_orders(sequelize, DataTypes);
@@ -30,7 +28,6 @@ function initModels(sequelize) {
   return {
     category,
     dev_category,
-    dev_kakao_user,
     dev_menu,
     dev_merchant,
     dev_orders,
