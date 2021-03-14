8798d075e1fc5c339210f90148507c8f9b76d169
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-03-14 14:27:19 +0900

    feat: Send order info according to status

--- server/api/index.js
+++ server/api/index.js
@@ -5,11 +5,13 @@ const users = require('./user');
 const menus = require('./menu');
 const categories = require('./category');
 const payments = require('./payment');
+const orders = require('./order');
 
 // api/users/~ 경로들은 users 모듈에서 사용
 api.use('/users', users.routes());
 api.use('/menus', menus.routes());
 api.use('/categories', categories.routes());
 api.use('/payments', payments.routes());
+api.use('/orders', orders.routes());
 
 module.exports = api;
