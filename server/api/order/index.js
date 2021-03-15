3d865e3f4c36600135f800e510b6a5e4af0805a9
jokbalkiller <55704603+jokbalkiller@users.noreply.github.com>
2021-03-16 00:36:57 +0900

    feat: 주문 스탯 변경 API

--- server/api/order/index.js
+++ server/api/order/index.js
@@ -6,5 +6,7 @@ const orders = new Router();
 
 // 주문 상태에 따라 주문 내역 조회
 orders.get('/:status', ordersCtrl.list);
+// 특정 주문 주문 상태 변경
+orders.patch('/:id', ordersCtrl.status);
 
 module.exports = orders;
