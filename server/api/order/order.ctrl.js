15dc9a0c0320b3c0ca3f9c25aecb47826238ede0
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-03-15 23:13:39 +0900

    feat: Send today's orders based on status

--- server/api/order/order.ctrl.js
+++ server/api/order/order.ctrl.js
@@ -1,12 +1,20 @@
+const {Sequelize, Op} = require('sequelize');
 const orderModel = require('../../models').dev_orders;
 
 exports.list = async (ctx) => {
     const {status} = ctx.params;
+    
     let list;
     try {
         list = await orderModel.findAll({
-            attributes: ['buyer_name', 'order_detail', 'date', 'order_stat'],
-            where: {order_stat: status},
+            attributes: ['buyer_name', [Sequelize.json('order_detail.cart'), 'order_detail'], 'date', 'order_stat'],
+            where: {
+                order_stat: status,
+                date: {
+                    [Op.gt]: new Date().setHours(0, 0, 0, 0), // midnight today
+                    [Op.lte]: new Date(), // now
+                },
+            },
             order: [['date', 'ASC']],
         });
     } catch (e) {
