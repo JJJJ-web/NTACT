ea59305287b7e9e3560cb5e598b2035951e5d07e
truebliss <s1871323@gmail.com>
2021-03-22 07:39:33 +0900

    refactor: 헤더탭수정

--- client/src/pages/Header.js
+++ client/src/pages/Header.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Row, Col} from 'antd';
+import {Row, Col, Layout, Menu} from 'antd';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
 import {MenuOutlined, ReadOutlined} from '@ant-design/icons';
@@ -8,17 +8,27 @@ function Header() {
     const [userName, setUserName] = useState(JSON.parse(sessionStorage.getItem('userInfo')).userName);
 
     return (
-        <Row>
-            <Col span={6}>
-                <ReadOutlined />메뉴
-            </Col>
-            <Col span={9}>
-                {userName}님 반갑습니다.
-            </Col>
-            <Col span={9}>
-                <MenuOutlined />결제 내역
-            </Col>
-        </Row>
+        <Layout>
+            <Menu theme="dark" mode="horizontal" theme="light" style={{lineHeight: '30px', textAlign: 'center', backgroundColor: '#FFF8EA'}}>
+
+                <Row>
+                    <Col span={6}>
+                        <Link to='/menu'>
+                            <ReadOutlined /> 메뉴판
+                        </Link>
+                    </Col>
+                    <Col span={12}>
+                        {userName}님 반갑습니다.
+                    </Col>
+                    <Col span={6}>
+                        <Link to='/payment/history'>
+                            <MenuOutlined /> 결제 내역
+                        </Link>
+                    </Col>
+                </Row>
+            </Menu>
+        </Layout>
+
     );
 }
 
