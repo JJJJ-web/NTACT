b813555460ce3768b16df945f4fd867ef681968f
jokbalkiller <55704603+jokbalkiller@users.noreply.github.com>
2021-03-30 13:25:47 +0900

    소켓 기본 연결

--- server/src/index.js
+++ server/src/index.js
@@ -9,6 +9,12 @@ const api = require('../api');
 const app = new Koa();
 const router = new Router();
 
+const io = require('socket.io')(Koa);
+
+io.on('connection', (socket) => {
+    console.log(`소켓 접속 ${socket}`);
+});
+
 // cors()를 사용하여 현재는 모든 경로에 대한 cors 접근을 허용했습니다. 
 // 추후 저희 프론트 경로만 접근 허용하는 옵션을 설정해야합니다.
 app.use(cors());
