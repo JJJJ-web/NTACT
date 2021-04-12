4b0b67e14d12ec3e3a01c742399f869e7d0f7c73
jokbalkiller <55704603+jokbalkiller@users.noreply.github.com>
2021-04-13 01:17:25 +0900

    feat: 로그인 성공시 토큰에 권한 부여

--- server/models/init-models.js
+++ server/models/init-models.js
@@ -1,40 +1,50 @@
-var DataTypes = require("sequelize").DataTypes;
-var _category = require("./category");
-var _dev_category = require("./dev_category");
-var _dev_menu = require("./dev_menu");
-var _dev_merchant = require("./dev_merchant");
-var _dev_orders = require("./dev_orders");
-var _dev_user = require("./dev_user");
-var _menu = require("./menu");
-var _user = require("./user");
+const DataTypes = require('sequelize').DataTypes;
+const _category = require('./category');
+const _dev_admin = require('./dev_admin');
+const _dev_category = require('./dev_category');
+const _dev_chef = require('./dev_chef');
+const _dev_menu = require('./dev_menu');
+const _dev_merchant = require('./dev_merchant');
+const _dev_orders = require('./dev_orders');
+const _dev_user = require('./dev_user');
+const _menu = require('./menu');
+const _user = require('./user');
 
 function initModels(sequelize) {
-  var category = _category(sequelize, DataTypes);
-  var dev_category = _dev_category(sequelize, DataTypes);
-  var dev_menu = _dev_menu(sequelize, DataTypes);
-  var dev_merchant = _dev_merchant(sequelize, DataTypes);
-  var dev_orders = _dev_orders(sequelize, DataTypes);
-  var dev_user = _dev_user(sequelize, DataTypes);
-  var menu = _menu(sequelize, DataTypes);
-  var user = _user(sequelize, DataTypes);
+    const category = _category(sequelize, DataTypes);
+    const dev_admin = _dev_admin(sequelize, DataTypes);
+    const dev_category = _dev_category(sequelize, DataTypes);
+    const dev_chef = _dev_chef(sequelize, DataTypes);
+    const dev_menu = _dev_menu(sequelize, DataTypes);
+    const dev_merchant = _dev_merchant(sequelize, DataTypes);
+    const dev_orders = _dev_orders(sequelize, DataTypes);
+    const dev_user = _dev_user(sequelize, DataTypes);
+    const menu = _menu(sequelize, DataTypes);
+    const user = _user(sequelize, DataTypes);
 
-  dev_menu.belongsTo(dev_category, { as: "category", foreignKey: "category_id"});
-  dev_category.hasMany(dev_menu, { as: "dev_menus", foreignKey: "category_id"});
-  dev_orders.belongsTo(dev_merchant, { as: "dev_merchant", foreignKey: "dev_merchant_id"});
-  dev_merchant.hasMany(dev_orders, { as: "dev_orders", foreignKey: "dev_merchant_id"});
-  menu.belongsTo(category, { as: "category", foreignKey: "category_id"});
-  category.hasMany(menu, { as: "menus", foreignKey: "category_id"});
+    dev_admin.belongsTo(dev_merchant, {as: 'dev_merchant', foreignKey: 'dev_merchant_id'});
+    dev_merchant.hasMany(dev_admin, {as: 'dev_admins', foreignKey: 'dev_merchant_id'});
+    dev_chef.belongsTo(dev_merchant, {as: 'dev_merchant', foreignKey: 'dev_merchant_id'});
+    dev_merchant.hasMany(dev_chef, {as: 'dev_chefs', foreignKey: 'dev_merchant_id'});
+    dev_menu.belongsTo(dev_category, {as: 'category', foreignKey: 'category_id'});
+    dev_category.hasMany(dev_menu, {as: 'dev_menus', foreignKey: 'category_id'});
+    dev_orders.belongsTo(dev_merchant, {as: 'dev_merchant', foreignKey: 'dev_merchant_id'});
+    dev_merchant.hasMany(dev_orders, {as: 'dev_orders', foreignKey: 'dev_merchant_id'});
+    menu.belongsTo(category, {as: 'category', foreignKey: 'category_id'});
+    category.hasMany(menu, {as: 'menus', foreignKey: 'category_id'});
 
-  return {
-    category,
-    dev_category,
-    dev_menu,
-    dev_merchant,
-    dev_orders,
-    dev_user,
-    menu,
-    user,
-  };
+    return {
+        category,
+        dev_admin,
+        dev_category,
+        dev_chef,
+        dev_menu,
+        dev_merchant,
+        dev_orders,
+        dev_user,
+        menu,
+        user,
+    };
 }
 module.exports = initModels;
 module.exports.initModels = initModels;
