2aaf19d7199544b47cc6a0162c09da8b2ee34aff
truebliss <s1871323@gmail.com>
2021-04-29 13:29:41 +0900

    feat: 로그인, 로그아웃

--- kitchen/src/containers/Layout/topbar/TopbarMenuLink.jsx
+++ kitchen/src/containers/Layout/topbar/TopbarMenuLink.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
-const TopbarMenuLinks = ({ title, icon, path }) => (
-  <Link className="topbar__link" to={path}>
-    <span className={`topbar__link-icon lnr lnr-${icon}`} />
-    <p className="topbar__link-title">{title}</p>
-  </Link>
-);
+const TopbarMenuLinks = ({ title, icon, path }) => {
+  function logOut(path) {
+    if(path === '/') {
+      sessionStorage.setItem(
+        'userInfo',
+        { userName: '', userRole: '' },
+      );
+    }
+  }
+
+  // logOut(path);
+
+  return (
+    <Link className="topbar__link" to={path}>
+      <span className={`topbar__link-icon lnr lnr-${icon}`} />
+      <p className="topbar__link-title">{title}</p>
+    </Link>
+  );
+};
 
 TopbarMenuLinks.propTypes = {
   title: PropTypes.string.isRequired,
