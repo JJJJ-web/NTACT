379063343b0db32fc158c3e1f281b955d333932b
truebliss <s1871323@gmail.com>
2021-05-07 17:44:27 +0900

    feat: Add 비회원로그인

--- client/src/component/Home.js
+++ client/src/component/Home.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { Button } from 'antd';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 function Home() {
   const btnSize = 'large';
   const btnShape = 'round';
+  const history = useHistory();
+
+  function guid() {
+    function s4() {
+      // eslint-disable-next-line no-bitwise
+      return ((1 + Math.random()) * 0x10000 | 0).toString(16).substring(1);
+    }
+    return `${s4() + s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
+  }
+
+  function nonMemberLogin() {
+    const UUID = guid();
+    sessionStorage.setItem(
+      'userInfo',
+      JSON.stringify({ userName: '비회원', userID: UUID, userRole: 'client' }),
+    );
+    history.push('/menu');
+  }
+
   return (
     <Layout>
       <div className="header">
@@ -15,9 +34,7 @@ function Home() {
         <Link to="/login">
           <Button type="primary" shape={btnShape} size={btnSize}>회원</Button>
         </Link>
-        <Link to="/menu">
-          <Button type="ghost" shape={btnShape} size={btnSize}>비회원</Button>
-        </Link>
+        <Button type="ghost" shape={btnShape} size={btnSize} onClick={nonMemberLogin}>비회원</Button>
       </span>
       <div className="footer">한성대학교 2021년 캡스톤디자인</div>
     </Layout>
