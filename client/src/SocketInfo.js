f597e3f4246fa68e278ea939c07c414146e43466
truebliss <s1871323@gmail.com>
2021-05-07 17:41:14 +0900

    chore: Change server ip

--- client/src/SocketInfo.js
+++ client/src/SocketInfo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:4000');
+const socket = io('http://13.209.109.91:4000');
 
 export default socket;
