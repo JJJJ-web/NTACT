832633d16832f99bf3fc0f183b28e2a76d10feed
truebliss <s1871323@gmail.com>
2021-05-11 01:23:20 +0900

    feat: Add jwt token verification

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,43 @@ import Router from './Routes/Router';
 import './App.less';
 import 'antd/dist/antd.css';
 // import io from 'socket.io-client';
+// eslint-disable-next-line import/order
+import { useHistory, useLocation } from 'react-router-dom';
 import socket from './SocketInfo';
+import loginInfo from './config/loginInfo.json';
+// eslint-disable-next-line import/order
+import jwt from 'jsonwebtoken';
 
 function App() {
+  const location = useLocation();
   const [socketEvents, setSocketEvents] = useState([]);
   const [condition, setCondition] = useState([]);
+  const history = useHistory();
 
   console.log(socket);
+  console.log(location.pathname);
+
+  if(location.pathname !== '/') { // 첫페이지 빼고
+    if (sessionStorage.getItem('jwt') != undefined) { // jwt 있는데
+      try { // jwt있는데 만료됐을때
+        const user = jwt.verify(
+          sessionStorage.getItem('jwt'),
+          loginInfo.jwt_password,
+        ); // 백에서 jwtToken받아옴
+        console.log(user);
+      } catch (e) {
+        if (e.name === 'TokenExpiredError') {
+          console.log('토큰시간 만료');
+          sessionStorage.removeItem('jwt');
+          history.push('/');
+        }
+      } finally {
+        console.log('finally');
+      }
+    } else if (sessionStorage.getItem('jwt') == undefined) { // jwt 없을때
+      history.push('/');
+    }
+  }
 
   socket.on('connect', () => {
     console.log('connection server');
--- client/src/component/Home.js
+++ client/src/component/Home.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Button } from 'antd';
 import styled from 'styled-components';
 import { Link, useHistory } from 'react-router-dom';
+import KakaoLogin from './KakaoLogin';
+import GoogleLogin from './GoogleLogin';
 
 function Home() {
-  const btnSize = 'large';
-  const btnShape = 'round';
   const history = useHistory();
 
   function guid() {
@@ -31,16 +31,38 @@ function Home() {
         <img height="100%" src="./ntact512.png" alt="logo" />
       </div>
       <span className="content">
-        <Link to="/login">
-          <Button type="primary" shape={btnShape} size={btnSize}>회원</Button>
-        </Link>
-        <Button type="ghost" shape={btnShape} size={btnSize} onClick={nonMemberLogin}>비회원</Button>
+        <GoogleLogin />
+        <KakaoLogin />
+        <NonMemberLoginStyle onClick={nonMemberLogin}>
+          <img src="./non-member.png" id="icon-img" alt="비회원로그인" />
+          비회원으로 시작하기
+        </NonMemberLoginStyle>
       </span>
       <div className="footer">한성대학교 2021년 캡스톤디자인</div>
     </Layout>
   );
 }
+const NonMemberLoginStyle = styled.button`
+  width: 300px;
+  height: 44px;
+  border-radius: 5px;
+  border-style: none;
+  margin: auto;
+  margin-bottom: 8px;
+  font-size: 12px;
+  color: #7f7f7f;
+  background-color: #ececec;
+  box-shadow: 0 2px 1px 0 rgba(155, 155, 155, 0.5);
+  display: flex;
+  align-items: center;
 
+  #icon-img {
+    margin-left: 50px;
+    margin-right: 26px;
+    margin-top: 6px;
+    margin-bottom: 6px;
+  }
+`;
 const Layout = styled.div`
   display: flex;
   justify-content: center;
