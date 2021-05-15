b355f1f2085768b130fce2e2d4ee54c64e15d4a4
truebliss <s1871323@gmail.com>
2021-05-16 01:16:28 +0900

    chore: Install redux-devtools-extension

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,12 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 import rootReducer from './store/reducers';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={store}>
