a7a143e8128ddb61070cb7af23b13bf3f3f5c6cf
reader-wh94 <sstt9884@gmail.com>
2021-05-19 22:56:58 +0900

    refactor: Add redux-persist

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import persistStore from 'redux-persist/es/persistStore';
+import { PersistGate } from 'redux-persist/integration/react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 import rootReducer from './store/reducers';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer);
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <PersistGate persistor={persistor}>
+      <Router>
+        <App />
+      </Router>
+    </PersistGate>
   </Provider>,
   document.getElementById('root'),
 );
