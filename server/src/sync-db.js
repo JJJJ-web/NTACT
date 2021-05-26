1acb864edad4f40c1516974fc0767cd74645f5ee
jokbalkiller <55704603+jokbalkiller@users.noreply.github.com>
2021-05-27 02:08:23 +0900

    fix: 레디스에 소켓 10분 저장 후 삭제 , ready room 생성

--- server/src/sync-db.js
+++ server/src/sync-db.js
@@ -3,6 +3,8 @@ const models = require('../models');
 module.exports = () => {
   const options = {
     force: process.env.NODE_ENV === 'test',
+    // 나중에 로그 안보이게 수정 예정
+    // logging: false,
   };
   return models.sequelize.sync(options);
 };
